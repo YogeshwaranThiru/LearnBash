30dce85355105d36da2a1ab092babdea4d75450b
YogeshwaranThiru <yogeshwaranthirunavukkarasu@gmail.com>
2024-05-27 23:31:06 +0530

    Bash_comments

---
+++ basic.sh
@@ -0,0 +1,14 @@
+#--> touch (to create a file)
+# code . --> to open visual studio. (.) open cuurent folder
+# pwd --> current working directory
+
+# to resolve DNS to IP address.
+# nslookup <domain>
+#top --> to check running process
+#htop --> to checck running procecss + thread (shft+H --> only thread or process)
+# to check line in  file
+# wc -l filename
+#man to check detail page of the command
+# eg: -w noof words
+# wc -w <filename>
+#df -h --> to check disc/storage usage
---
+++ curl.sh
@@ -0,0 +1,9 @@
+#curl --> to get information of the webpage
+# jq --> to view information in terminal window readable
+# eg: curl <domain> | jq
+# pipe --> give input to next tast from previous output
+# ls -ltrh --> l-print by list, t-time, r- reverseorder, h- human readable size.
+#tail -f <domain> --> tail- to check file latest log, -f is flow  
+#grep --> to find pattern in the file
+#eg: grep -e <pattern> 
+# grep -e <pattern> -B lineno -A lineno
